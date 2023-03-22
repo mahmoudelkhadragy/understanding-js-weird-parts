55069406b93fd1b2a5fae04e4a83ea00ff7a30c5
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-03-22 16:55:22 +0200

    execution context

---
+++ section1/execution-context.js
@@ -0,0 +1,26 @@
+/*
+  Syntax Parser: 
+    a program that reads your code and determines what it does and if its grammer is valid.
+  -------
+  => your code is not magic someone else wrote a program to translate it for the computer
+  -------
+
+
+  Lexical Environment:
+    where something sits physically in the code your write
+    or
+    where the code is written in your code
+  
+  -------
+  => where you write the piece of code is important 
+  -------
+
+
+  Name/Value pair: 
+    a name which maps to a unique value
+    the name may be defined more than once, but only can have one value in any given context.
+
+  Object:
+    in js object is collection of name value pairs
+
+*/
